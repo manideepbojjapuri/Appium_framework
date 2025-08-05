a80c207212a41529598cb4c762ce41782881e6ce
manideep <manideep.bojjapuri@360f.com>
2025-08-05 23:25:27 +0530

    code

---
+++ src/test/java/automation/tests/testdata_interfaces/ConnectToFastrackApp.java
@@ -0,0 +1,11 @@
+package automation.tests.testdata_interfaces;
+
+public interface ConnectToFastrackApp {
+
+    String otpPopupTitle = "Allow Fastrack Smart to read the message below and enter the code?";
+    String permissionsPopupTitle = "Permissions";
+    String cameraPermisssionPopupTitle = "Allow Fastrack Smart to take pictures and record video?";
+    String locationPermissionPopupTitle = "Allow Fastrack Smart to access this device's location?";
+    String bluetoothPermissionPopupTitle = "Fastrack Smart wants to turn on Bluetooth.";
+
+}
